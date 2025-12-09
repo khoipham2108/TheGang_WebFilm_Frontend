c05abd8d312c26a2e235e2c69e075f547326e123
Pham Nguyen Anh Khoi <phamnguyenanhkhoi21082005@gmail.com>
2025-12-10 01:22:12 +0700

    Fix logo path

--- src/Componets/MainPageDesign/Header.tsx
+++ src/Componets/MainPageDesign/Header.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import logo from "../../../public/images/logo.png";
+import logo from "/images/logo.png";
 import { 
   HiHome, 
   HiMagnifyingGlass, 
